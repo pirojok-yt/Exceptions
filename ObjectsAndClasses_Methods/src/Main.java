3af38974686160345b5ac7c2a6cc305a4e3817b1
Tamerlan Mamatov <aaltynai@mail.ru>
2022-12-02 14:40:48 +0000

    Update Main.java

--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         Basket basket = new Basket();
-        basket.add("Milk", 40);
-        basket.print("Milk");
+        basket.add("Milk", 40, 50);
+        basket.add("Latte", 30, 20);
+        basket.print("Корзина");
     }
 }
