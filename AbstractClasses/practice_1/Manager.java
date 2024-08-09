df673a14cec891e8b178a200cafc549431d89e6b
Tamerlan Mamatov <aaltynai@mail.ru>
2024-08-09 16:39:48 +0300

    Update Manager.java

--- AbstractClasses/practice_1/Manager.java
+++ AbstractClasses/practice_1/Manager.java
@@ -1,10 +1,13 @@
 public class Manager extends Employee {
+
     double revenue;
-    public Manager(double salary, double revenue){
+    public Manager(double salary, double revenue, Company company){
+        super(company);
         this.salary = salary + revenue*0.05;
     }
 
     public String toString() {
         return "Manager with salary " + String.valueOf(salary);
     }
+
 }
