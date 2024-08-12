d03b5c3e1e5dca48876812d97ddba1b9f0aa4240
Tamerlan Mamatov <aaltynai@mail.ru>
2024-08-12 16:49:08 +0300

    Update TopManager.java

--- AbstractClasses/practice_1/TopManager.java
+++ AbstractClasses/practice_1/TopManager.java
@@ -1,7 +1,7 @@
 public class TopManager extends Employee {
     public TopManager(double salary, Company company){
         super(company);
-        if(getIncome() > 10000000)
+        if(company.getIncome() > 10000000)
             this.salary =  salary + 1.5*salary;
         else
             this.salary = salary;
